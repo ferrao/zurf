10fc8c403d0f66cda95dd51596fd7572c687edd1
Rui Ferrao <ferrao@eixodigital.com>
2014-11-20 21:24:09 +0000

    card layout on detail view

--- frontend/zurf/www/js/login-ctrl.js
+++ frontend/zurf/www/js/login-ctrl.js
@@ -1,12 +1,8 @@
 var app = angular.module('zurf');
 
 app.controller('LoginCtrl', function($scope, $state) {
-
   $scope.login = function(user) {
-
     console.log('Login', user);
     $state.go('tab.break-list-fav');
-
   };
-
 });
--- frontend/zurf/www/js/surf-break-ctrl.js
+++ frontend/zurf/www/js/surf-break-ctrl.js
@@ -1,10 +1,7 @@
 var app = angular.module('zurf');
 
 app.controller('BreakListCtrl', function($scope, $state, surfBreakService) {
-
-
   surfBreakService.surfBreaks().then(function(data) {
-
       if ($state.is('tab.break-list')) {
         $scope.breaks = data;
       } else {
@@ -12,23 +9,15 @@ app.controller('BreakListCtrl', function($scope, $state, surfBreakService) {
           return value.favorite;
         });
       }
-
     });
-
 });
 
 app.controller('BreakDetailCtrl', function($scope, $stateParams, surfBreakService) {
-
   $scope.fav = false;
-
   surfBreakService.getSurfBreak($stateParams.id).then(function(data) {
-
       $scope.break = data;
-
-    });
-
+  });
   $scope.clickFav = function() {
     $scope.fav = $scope.fav ? false : true;
   };
-
 });
