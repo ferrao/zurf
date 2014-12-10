25c3b16fe501842a6fa39f21516ce84c40e2096c
Rui Ferrao <ferrao@eixodigital.com>
2014-12-10 19:15:48 +0000

    Facebook login

--- frontend/www/js/login-ctrl.js
+++ frontend/www/js/login-ctrl.js
@@ -1,10 +1,27 @@
 var app = angular.module('zurf');
 
-app.controller('LoginCtrl', function($scope, $state) {
+app.controller('LoginCtrl', function($scope, $state, OpenFB) {
 
     $scope.login = function(user) {
         console.log('Login', user);
         $state.go('tab.break-list-fav');
+    };
+
+    $scope.fbLogin = function() {
+
+        OpenFB.login(fbLoginCallback, {
+            scope: 'email'
+        });
+
+    };
+
+    var fbLoginCallback = function(response) {
+
+        if (response.status === 'connected') {
+            console.log('Facebook login succeeded');
+        } else {
+            console.log('Facebook login failed');
+        }
     };
 
 });
